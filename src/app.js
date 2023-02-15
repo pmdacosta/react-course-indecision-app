56e73451b3b893c9da16b45061be1deac759f004
CabralMoncada <pedroalexandrino1729@protonmail.com>
2023-02-15 14:29:45 +0000

    moved counter to examples in playground

--- src/app.js
+++ src/app.js
@@ -31,37 +31,5 @@ function getLocation(location) {
     }
 }
 
-let count = 0;
-const addOne = () => {
-    count++;
-    render();
-    console.log(count);
-}
-
-const subOne = () => {
-    count > 0 && count--;
-    render();
-    console.log(count);
-}
-
-const reset = () => {
-    count = 0;
-    render();
-    console.log(count);
-}
-
-const render = () => {
-    const templateTwo = (
-        <div>
-            <h1>Count: {count}</h1>
-            <button onClick={addOne}>+1</button>
-            <button onClick={subOne}>-1</button>
-            <button onClick={reset}>reset</button>
-        </div>
-    );
-    const appRoot = document.getElementById('app');
-
-    ReactDOM.render(templateTwo, appRoot);
-}
-
-render();
+const appRoot = document.getElementById('app');
+ReactDOM.render(template, appRoot);
