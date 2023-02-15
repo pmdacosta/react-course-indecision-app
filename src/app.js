9b36a88831bbe0c4a65f631ef379b9631328b3e6
CabralMoncada <pedroalexandrino1729@protonmail.com>
2023-02-15 12:03:04 +0000

    module 3 - lesson 12

--- src/app.js
+++ src/app.js
@@ -34,26 +34,34 @@ function getLocation(location) {
 let count = 0;
 const addOne = () => {
     count++;
+    render();
     console.log(count);
 }
 
 const subOne = () => {
     count > 0 && count--;
+    render();
     console.log(count);
 }
 
 const reset = () => {
     count = 0;
+    render();
     console.log(count);
 }
-const templateTwo = (
-    <div>
-        <h1>Count: {count}</h1>
-        <button onClick={addOne}>+1</button>
-        <button onClick={subOne}>-1</button>
-        <button onClick={reset}>reset</button>
-    </div>
-);
-const appRoot = document.getElementById('app');
 
-ReactDOM.render(templateTwo, appRoot);
+const render = () => {
+    const templateTwo = (
+        <div>
+            <h1>Count: {count}</h1>
+            <button onClick={addOne}>+1</button>
+            <button onClick={subOne}>-1</button>
+            <button onClick={reset}>reset</button>
+        </div>
+    );
+    const appRoot = document.getElementById('app');
+
+    ReactDOM.render(templateTwo, appRoot);
+}
+
+render();
