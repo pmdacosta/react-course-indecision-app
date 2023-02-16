01202165502f3ee53edbe16afd4383d1a3043bba
CabralMoncada <pedroalexandrino1729@protonmail.com>
2023-02-16 15:47:24 +0000

    module 4 - lesson 5

---
+++ src/app.js
@@ -0,0 +1,51 @@
+class Header extends React.Component {
+  render() {
+    return (
+      <div>
+        <h1>Indecision App</h1>
+        <h2>Put your life in the hands of a computer.</h2>
+      </div>
+    );
+  }
+}
+
+class Action extends React.Component {
+  render() {
+    return (
+      <div>
+        <button>What should I do?</button>
+      </div>
+    );
+  }
+}
+
+class Options extends React.Component {
+  render() {
+    return (
+      <div>
+        <p>Options component here</p>
+      </div>
+    );
+  }
+}
+
+class AddOption extends React.Component {
+  render() {
+    return (
+      <div>
+        <p>AddOption component here</p>
+      </div>
+    );
+  }
+}
+
+const jsx = (
+  <div>
+    <Header />
+    <Action />
+    <Options />
+    <AddOption />
+  </div>
+)
+
+ReactDOM.render(jsx, document.getElementById('app'));
