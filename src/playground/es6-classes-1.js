d19c7fb54ef24b5424c2c6c78f8191981eba35f1
CabralMoncada <pedroalexandrino1729@protonmail.com>
2023-02-16 15:16:48 +0000

    module 4 - lesson 4

--- src/playground/es6-classes-1.js
+++ src/playground/es6-classes-1.js
@@ -5,7 +5,7 @@ class Person {
     }
 
     getGreeting() {
-        return `Hello ${this.name}!`;
+        return `Hello I'm ${this.name}!`;
     }
 
     getDescription() {
@@ -13,14 +13,56 @@ class Person {
     }
 }
 
+class Student extends Person {
+    constructor(name, age, major) {
+        super(name, age);
+        this.major = major;
+    }
+
+    hasMajor() {
+        return !!this.major;
+    }
+
+    getDescription() {
+        let desc = super.getDescription();
+        if (this.hasMajor())
+            desc += ` He is majoring in ${this.major}`;
+        return desc;
+    }
+}
+
+class Traveler extends Person {
+    constructor(name, age, homeLocation) {
+        super(name, age);
+        this.homeLocation = homeLocation;
+    }
+
+    hasHomeLocation() {
+        return !!this.homeLocation;
+    }
+
+    getGreeting() {
+        let greeting = super.getGreeting();
+        if (this.hasHomeLocation())
+            greeting += ` I'm visiting from ${this.homeLocation}.`;
+        return greeting;
+    }
+}
+
 const people = [
     new Person("Pedro", 25),
     new Person("Julio"),
-    new Person(),
+    new Student(),
+    new Student("Newton", 17, "Physics"),
+    new Traveler("Carlos", 12, "Porto"),
+    new Traveler(undefined,undefined,"nowhere")
 ];
 
 const jsx = <div>
-    {people.map((person) => <p>{person.getDescription()}</p>)}
+    {people.map((person) => <p key={Math.floor(Math.random() * 1000)}>{person.getDescription()}</p>)}
 </div>;
 
 ReactDOM.render(jsx, document.getElementById('app'));
+
+console.log(people[4].getGreeting());
+console.log(people[5].getGreeting());
