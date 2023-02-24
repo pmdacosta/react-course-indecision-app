20abf5576bd162d31e74dd9691cff02fb790c782
Pedro Costa <pedro@example.com>
2023-02-24 11:30:51 +0000

    module 6 - lesson 8

--- src/app.js
+++ src/app.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-// const template = <h1>Hello World</h1>;
-const template = React.createElement('h1', {color: 'red'}, 'Hello World');
+const template = <h1 color="red">Hello World</h1>;
+//const template = React.createElement('h1', {color: 'red'}, 'Hello World');
 
 ReactDOM.render(template, document.getElementById('app'));
 
